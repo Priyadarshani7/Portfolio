b804d6bddcd55be3aba49c6741f1ce131c7cef38
Priyadarshani Kalbandhe <priyadarshanik07@gmail.com>
2023-03-31 22:35:45 +0530

    Portfolio

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import './App.css';
+import photo from './pic.jpg';
+
+function App() {
+  return (
+    <div className="App">
+      <div >
+            <h1>Priyadarshani Kalbandhe👩🏻‍💻</h1>
+            <div className='piyuk'><img src={photo} alt="Girl in a jacket" width="500" height="600"/>;</div>
+             </div>
+           
+           
+     
+      <div id="intro">
+        <p >I am a Second Year Computer Engineering student pursuing B-Tech at Pune University.
+I am into Web development and currently, I am learning React js.
+My Techstack include HTML, CSS, Javascript, React js, C and C++</p>
+    </div>
+    <div id="c">
+   <h3>Community Work🎓</h3>
+   <p>I am also a part of various Community in college as well as outside.I am a part of Ui Path Community in my college as Core Team member as well as a part of an online Community named OreoDrioders where I am contributing in design team</p>
+    </div>
+    <div id="project">
+    <h3>My Projects🏷️</h3>
+   
+   <li>To-do-List    <a href="https://to-do-list-by-piyu.netlify.app/">    https://to-do-list-by-piyu.netlify.app/ </a></li>
+   
+   <li>Clock Website    <a href="https://clockprojectbypiyu.netlify.app/">  https://clockprojectbypiyu.netlify.app/</a></li>
+  
+  <li>Zomato Landing page     <a href="https://zomato-page-bypiyu.netlify.app/">   https://zomato-page-bypiyu.netlify.app/</a></li>
+   
+ </div>
+ <div id="social">
+    <h3>Social Handles📲</h3>
+    
+        <li>Github   <a href="https://github.com/Priyadarshani7"> Priyadarshani7</a> </li>
+        
+       <li>Twitter  <a href="https://twitter.com/priyadarshani7k"> @priyadarshani7k</a> </li>
+       
+       <li> Linked-In  <a href="https://www.linkedin.com/in/priyadarshani-kalbandhe-990643235/">Priyadarshani Kalbandhe</a></li>
+       
+       <li> Email  <a href="https://mail.google.com/mail/u/0/#inbox"> priyadarshanik07@gmail.com</a></li>
+    
+ </div>
+      </div>
+)};
+
+  
+
+
+export default App;
